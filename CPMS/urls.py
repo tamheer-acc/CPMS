e9504bd8c31432899cd2bef73494e27c49d9a255
Walaa <wloow159@gmail.com>
2025-12-21 10:22:28 +0300

    .

--- CPMS/urls.py
+++ CPMS/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from CPMS_app.views import (AllDepartmentsView, AllPlansView,PlanDetailsview,CreatePlanView,
-                            UpdatePlanView, DeletePlanView,
-                            CreateGoalView, UpdateGoalView,DeleteGoalView)
-  # AllGoalsView, GoalDetailsview,        
+                            UpdatePlanView, DeletePlanView, AllGoalsView, GoalDetailsview,
+                            CreateGoalView, UpdateGoalView,DeleteGoalView,AllNotesView,
+                            NoteDetailsview, CreateNoteView, UpdateNoteView, DeleteNoteView)
+   
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('departmets',AllDepartmentsView.as_view(), name='departments_list'),
@@ -28,10 +29,15 @@ urlpatterns = [
     path('plans/create', CreatePlanView.as_view(), name='create_plan'),
     path('plans/<int:pk>/update', UpdatePlanView.as_view(), name='update_plan'),
     path('plans/<int:pk>/delete', DeletePlanView.as_view(), name='delete_plan'),
-   # path('goals', AllGoalsView.as_view(), name='goals_list'),
-   # path('goals/<int:pk>/detail', GoalDetailsview.as_view(), name='goal_detail'),
+    path('goals', AllGoalsView.as_view(), name='goals_list'),
+    path('goals/<int:pk>/detail', GoalDetailsview.as_view(), name='goal_detail'),
     path('goals/add', CreateGoalView.as_view(), name='add_goal'),
     path('goals/<int:pk>/update', UpdateGoalView.as_view(), name='update_goal'),
     path('goals/<int:pk>/delete', DeleteGoalView.as_view(), name='delete_goal'),
+    path('notes', AllNotesView.as_view(), name='notes_list'),
+    path('notes/<int:pk>/detail', NoteDetailsview.as_view(), name='note_detail'),
+    path('notes/create', CreateNoteView.as_view(), name='create_note'),
+    path('notes/<int:pk>/update', UpdateNoteView.as_view(), name='update_note'),
+    path('notes/<int:pk>/delete', DeleteNoteView.as_view(), name='delete_note'),
 
 ]
--- CPMS_app/views.py
+++ CPMS_app/views.py
@@ -196,12 +196,31 @@ class DeletePlanView(DeleteView):
 # ---------------------------
 
 #LoginRequiredMixin, ListView
-#class AllGoalsView(ListView): 
-    
+class AllGoalsView(ListView): 
+    model = StrategicGoal 
+    template_name = 'goals_list.html'
+    context_object_name = 'goals'
+        
+    def get_queryset(self):
+     user = self.request.user
+     role = user.role.role_name
+
+     if role == 'GM':
+        return StrategicGoal.objects.all()
+     elif role in ['M','CM']:
+        return StrategicGoal.objects.filter(department = user.department)
+     elif role == 'E':
+        return StrategicGoal.objects.filter(initiative__userinitiative__user = user).distinct()
+     
+     return StrategicGoal.objects.none()
+
+        
 #LoginRequiredMixin, DetailView 
-#class GoalDetailsview(DetailView):
-    
-          
+class GoalDetailsview(DetailView):
+    model = StrategicGoal 
+    template_name = 'goal_detail.html'
+    context_object_name = 'goal'
+        
 #LoginRequiredMixin, UserPassesTestMixin, CreateView            
 class CreateGoalView(CreateView):
     model = StrategicGoal
@@ -224,22 +243,145 @@ class UpdateGoalView(UpdateView):
     fields = ['goal_title', 'description', 'start_date', 'end_date', 'goal_status', 'goal_priority']
     template_name = 'goal_form.html'
     success_url = reverse_lazy('')
-    
-    def test_func(self):
-        return self.request.user.role.role_name in ['CM','M'] 
+
+    def get_queryset(self):
+        user = self.request.user
+        role = user.role.role_name
+
+        if role in ['M', 'CM']:
+            return StrategicGoal.objects.filter(department=user.department)
+        return StrategicGoal.objects.none()
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.get_queryset().filter(pk=self.kwargs['pk']).exists():
+            raise PermissionDenied("ليس لديك صلاحية تعديل هذا الهدف.")
+        return super().dispatch(request, *args, **kwargs)
+     
     
 
 class DeleteGoalView(DeleteView):
     model = StrategicGoal
-    template_name = 'goal_form.html'
+    template_name = 'goal_confirm_delete.html'
     success_url = reverse_lazy('')
-    
-    def test_func(self):
-        return self.request.user.role.role_name in ['CM','M'] 
+
+    def get_queryset(self):
+        user = self.request.user
+        role = user.role.role_name
+
+        if role in ['M', 'CM']:
+            return StrategicGoal.objects.filter(department=user.department)
+        return StrategicGoal.objects.none()
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.get_queryset().filter(pk=self.kwargs['pk']).exists():
+            raise PermissionDenied("ليس لديك صلاحية حذف هذا الهدف.")
+        return super().dispatch(request, *args, **kwargs)
     
 # ---------------------------
 #  Note View
 # ---------------------------
+#LoginRequiredMixin, ListView
+class AllNotesView(ListView): 
+    model = Note 
+    template_name = 'notes_list.html'
+    context_object_name = 'notes'
+        
+    def get_queryset(self):
+        user = self.request.user
+        role = user.role.role_name
+
+        if role == 'GM':
+            #كل الملاحظات اللي ارسلها
+            return Note.objects.filter(user=user)
+        elif role in ['M','CM']:
+            #كل الملاحظات اللي ارسلها واللي اسلتمها من المدير العام
+            return Note.objects.filter(user=user,department=user.department)
+        elif role == 'E':
+            #كل الملاحظات اللي ارسلها واللي استلمهاعلى نفس المبادرة أو ارسلها له المدير 
+            return Note.objects.filter(user=user,initiative__userinitiative__user=user,department=user.department).distinct()
+
+        return Note.objects.none()
+        
+#LoginRequiredMixin, DetailView 
+class NoteDetailsview(DetailView):
+    model = Note 
+    template_name = 'note_detail.html'
+    context_object_name = 'note'
+
+    """اعتقد مالها داعي يمكن احذفها
+    def get_queryset(self):
+      
+        user = self.request.user
+        return Note.objects.filter(sender=user, receiver=user, initiative__userinitiative__user=user).distinct()
+
+            def dispatch(self, request, *args, **kwargs):
+        if not self.get_queryset().filter(pk=self.kwargs['pk']).exists():
+            raise PermissionDenied("ليس لديك صلاحية عرض هذه الملاحظة.")
+        return super().dispatch(request, *args, **kwargs)
+     """
+
+        
+#LoginRequiredMixin, UserPassesTestMixin, CreateView            
+class CreateNoteView(CreateView):
+    model = Note
+    fields = ['content', 'initiative', 'department', 'receiver']
+    template_name = 'note_form.html'
+    success_url = reverse_lazy('')
+    
+    #هذا عشان يسجل اسم المستلم من dropdown مخصصة
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        user = self.request.user
+        role = user.role.role_name
+       
+        if role == 'GM':
+            form.fields['receiver'].queryset = User.objects.filter(role__role_name='M')
+        elif role in ['M','CM']:
+            form.fields['receiver'].queryset = User.objects.filter(department=user.department, role__role_name='E')
+        elif role == 'E':
+            form.fields['receiver'].queryset = User.objects.filter(
+                userinitiative__initiative__in=Initiative.objects.filter(
+                    userinitiative__user=user)
+                      ).distinct()
+        return form
+
+    def form_valid(self, form):
+        form.instance.sender = self.request.user
+        return super().form_valid(form) 
+
+   
+#LoginRequiredMixin, UserPassesTestMixin, CreateView   
+class UpdateNoteView(UpdateView):
+    model = Note
+    fields = ['content', 'initiative', 'department', 'receiver']
+    template_name = 'note_form.html'
+    success_url = reverse_lazy('')
+
+    def get_queryset(self):
+        return Note.objects.filter(sender=self.request.user)
+    
+    def dispatch(self, request, *args, **kwargs):
+        if not self.get_queryset().filter(pk=self.kwargs['pk']).exists():
+            raise PermissionDenied("ليس لديك صلاحية تعديل هذه الملاحظة.")
+        return super().dispatch(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        form.instance.sender = self.request.user
+        return super().form_valid(form)
+    
+
+class DeleteNoteView(DeleteView):
+    model = Note
+    template_name = 'note_confirm_delete.html'
+    success_url = reverse_lazy('')
+
+    def get_queryset(self):
+        return Note.objects.filter(sender=self.request.user)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.get_queryset().filter(pk=self.kwargs['pk']).exists():
+            raise PermissionDenied("ليس لديك صلاحية حذف هذه الملاحظة.")
+        return super().dispatch(request, *args, **kwargs)
 
 # ---------------------------
 #  Log View
