1f41cebadb7786ab2ef11b3a85d6c76b05efa18f
renad <xirnzsixteen@hotmail.com>
2026-01-01 09:48:19 +0300

    add window popup (still not working)

--- CPMS_app/static/CPMS_app/app.js
+++ CPMS_app/static/CPMS_app/app.js
@@ -1,11 +1,11 @@
 
-const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=400, height=300,left=100,top=100`;
+const params = "scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=400,height=300,left=100,top=100";
 
 function openPopup(url){
 
     window.open(
         url,        //url to open
-        "_blank",   //target: new window
+        "popupWindow",   //target: new window
         params      //features
 
     );
